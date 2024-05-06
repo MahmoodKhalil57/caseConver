ed020b5aa090ad5f5931fd20982c773ac9a8a871
MahmoodKhalil57 <46604753+MahmoodKhalil57@users.noreply.github.com>
2024-05-06 09:00:44 +0100

    Initialize v2.1.0_daisy

---
+++ src/routes/(app)/(index)/+page.svelte
@@ -0,0 +1,3 @@
+<section class="w-full flex flex-col justify-center items-center">
+	<div class="flex-col gap-6 flex-center">Hello</div>
+</section>
---
+++ src/routes/(app)/+layout.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import Footer from '$lib/components/layout/footer.svelte';
+	import Header from '$lib/components/layout/header.svelte';
+	import Navigation from '$lib/components/layout/navigation.svelte';
+	import DrawerContainer from '$lib/components/ui/DrawerContainer.svelte';
+</script>
+
+<DrawerContainer closeBtn={true} drawerClass="bg-base-100" drawerId="main-menu-drawer">
+	<svelte:fragment slot="main">
+		<Header />
+		<main class="z-10 relative flex justify-center w-full min-h-page">
+			<slot />
+		</main>
+		<Footer />
+	</svelte:fragment>
+	<Navigation menu slot="drawerContent" />
+</DrawerContainer>
