ff9087eb386f2f017f642f3e42b23e0ae9eda109
MahmoodKhalil57 <mahmoodihabk@gmail.com>
2024-05-06 14:27:18 +0100

    asf

--- src/lib/components/template/caseConverter.svelte
+++ src/lib/components/template/caseConverter.svelte
@@ -14,7 +14,7 @@
 	<div class="w-full flex flex-wrap items-center justify-center gap-2">
 		{#each Object.entries(actions) as [title, action]}
 			<ButtonDefault onClick={() => (textareaValue = action(textareaValue))}>
-				{action ? action(title) : title}
+				{actions.text_case ? actions.text_case(action(title)) : title}
 			</ButtonDefault>
 		{/each}
 	</div>
